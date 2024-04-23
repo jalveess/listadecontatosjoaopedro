87140f52735d2257e799a26c2f02abb1256c3ec7
jalveess <167465724+jalveess@users.noreply.github.com>
2024-04-23 16:37:26 -0300

    criada a classe contato
    criada a classe contato.cs e os métodos construtores da classe e suas propriedades.

---
+++ lista de contato/contato.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_de_contato
+{
+    internal class contato
+    {
+       //Declaração de variáveis.
+        private string nome;
+        private string sobrenome;
+        private string telefone;
+    
+}     //Propriedades. São os métodos "get" e "set"
+      // get = leitura.
+      // set= definição
+      // é possível ser somente leitura, ou somente definição..
+       public string Nome
+       {
+           get
+           {
+            return nome;
+           }
+        set
+        {
+            nome = value;
+        }
+    }
+        public string Sobrenome
+        {   
+        get
+        {
+            return sobrenome;
+
+        }
+        set
+        {
+            if (value.Lenght == 13)
+            {
+                telefone = value;
+            }
+            else
+            {
+
+            }
+
+
+
+
+         
+
