c993bbb32125b31b45dd32ef4c55c2ceae537450
jalveess <167465724+jalveess@users.noreply.github.com>
2024-04-30 16:33:58 -0300

    lista de contato
    comando ler

--- lista de contato/Form1.cs
+++ lista de contato/Form1.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
+using System.Drawing.Text;
 namespace lista_de_contato
 {
     public partial class Form1 : Form
@@ -15,6 +16,63 @@ namespace lista_de_contato
         public Form1()
         {
             InitializeComponent();
+        }
+        private contato[] listadecontato = new contato[1];
+
+        private void Escrever(contato contato)
+        {
+            StreamWriter escrever = new StreamWriter("Contatos.txt");
+
+            escrever.WriteLine(listaDeContatos.Lenght + 1);
+            escrever.WriteLine(contato.Nome);
+            escrever.WriteLine(contato.Sobrenome);
+            escrever.WriteLine(contato.Telefone);
+
+            for (int x = 0; x < listaDeContatos.Lenght; x++)
+            {
+                escrever.WriteLine(listadecontato[x].Nome);
+                escrever.WriteLine(listadecontato[x].Sobrenome);
+                escrever.WriteLine(listaDeContatos[x].Telefone);
+            }
+
+            escrever.Close();
+        }
+
+
+        private void Ler()
+        {
+            StreamReader ler = new StreamReader("Contatos.txt");
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) 
+
+        
+       
+        {
+         
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Cria um objeto da classe contato.
+            contato contato = new contato();
+
+            contato.Nome = TextBoxNome.Text;
+            contato.Sobrenome = textBoxSobrenome.Text;
+            contato.telefone = textBoxTelefone.Text;
+
+            ListBoxContatos.Items.Add(contato); 
+
+
+
+
         }
     }
 }
--- lista de contato/contato.cs
+++ lista de contato/contato.cs
@@ -9,43 +9,85 @@ namespace lista_de_contato
 {
     internal class contato
     {
-       //Declaração de variáveis.
+        //Declaração de variáveis.
         private string nome;
         private string sobrenome;
         private string telefone;
-    
-}     //Propriedades. São os métodos "get" e "set"
-      // get = leitura.
-      // set= definição
-      // é possível ser somente leitura, ou somente definição..
-       public string Nome
-       {
-           get
-           {
-            return nome;
-           }
-        set
+
+        //Propriedades. São os métodos "get" e "set"
+        // get = leitura.
+        // set= definição
+        // é possível ser somente leitura, ou somente definição..
+        public string Nome
         {
-            nome = value;
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                nome = value;
+            }
         }
-    }
         public string Sobrenome
-        {   
-        get
         {
-            return sobrenome;
+            get
+            {
+                return sobrenome;
 
+            }
+            set
+            {
+                if (value.Length == 13)
+                {
+                    telefone = value;
+                }
+                else
+                {
+
+                }
+            }
         }
-        set
+        //Construtor de classe.
+        
+        public contato()
         {
-            if (value.Lenght == 13)
-            {
-                telefone = value;
-            }
-            else
-            {
+            Nome = "Cláudio";
+            Sobrenome = "Genésio II";
+            telefone = "(11) 98888 - 7777";
+        }   
+        // Sobrecarga do método construtor
+        public contato (string nome, string sobrenome, string telefone)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            telefone = telefone;
+        }
 
-            }
+
+
+    }//ToString() pertence a classe object.
+    //Todas as classes são filhas de object (Herança).
+    // "override" sobrescreve o método da classe pai (polímorfísmo).
+
+    public override string tostring()
+    {
+        //Inicializa a variável como vazia.
+        string saída = string.Empty;
+        saída += String.Format("{0} {1} ", Nome, Sobrenome);
+        saída += String.Format("({0}) {1}-{2} ",
+            Telefone.Substring(0, 2),
+            Telefone.Substring(3, 5),
+            Telefone.Substring(7, 4);
+
+        return saída;
+    
+      
+        
+       
+        
+    }
+}
 
 
 
